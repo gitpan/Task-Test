6bd22009e1885ee08bc0925f4f0a4ff6b7c31278
גאבור סבו - Gábor Szabó <gabor@pti.co.il>
2014-05-25 10:09:21 +0000

    Import of SZABGAB/Task-Test-0.02 from CPAN.
    gitpan-cpan-distribution: Task-Test
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         SZABGAB/Task-Test-0.02.tar.gz
    gitpan-cpan-author:       SZABGAB
    gitpan-cpan-maturity:     released

--- lib/Task/Test.pm
+++ lib/Task/Test.pm
@@ -3,14 +3,22 @@ use strict;
 use warnings;
 use 5.008005;
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 =head1 SYNOPSIS
 
-Tast::Test - an easy way to install lots of test related modules
+Task::Test - an easy way to install lots of modules needed for testing
+
+=head1 DESCRIPTION
+
+For explanation on using some of the modules installed by this module check out
+the slides of L<Test Automaion Using Perl|http://edumaven.com/test-automation-using-perl/>.
+
+=head1 AUTHOR
+
+L<Gabor Szabo|http://szabgab.com/>
 
 =cut
-
 
 
 1;
